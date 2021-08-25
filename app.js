5c6241330cbd02b90f7dc34b54a07c26d499ec5d
abdulazizcode <tajikcoder@gmail.com>
2021-08-26 06:34:08 +0800

    encrypting password

--- app.js
+++ app.js
@@ -1,8 +1,10 @@
 const express = require('express');
 const ejs = require('ejs');
+const mongoose = require('mongoose');
+const encrypt = require('mongoose-encryption');
+
 const app = express();
 const port = 3000;
-const mongoose = require('mongoose');
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true })); //Parse URL-encoded
@@ -11,7 +13,7 @@ app.set('view engine', 'ejs');
 
 //    Connecting to MongoDB
 mongoose
-    .connect('mongodb://localhost:27017/wikiDb', {
+    .connect('mongodb://localhost:27017/userDB', {
         useNewUrlParser: true,
         useUnifiedTopology: true
     })
@@ -23,6 +25,17 @@ mongoose
         console.log(err);
     });
 
+const userSchema = new mongoose.Schema({
+    email: String,
+    password: String
+});
+
+//encrypt password
+const secret = 'Thisisourlittlesecret.';
+userSchema.plugin(encrypt, { secret: secret, encryptedFields: ['password'] });
+
+const User = new mongoose.model('User', userSchema);
+
 app.get('/', (req, res) => {
     res.render('home');
 });
@@ -31,6 +44,39 @@ app.get('/login', (req, res) => {
 });
 app.get('/register', (req, res) => {
     res.render('register');
+});
+
+app.post('/login', (req, res) => {
+    const username = req.body.username;
+    const password = req.body.password;
+    //Comparing if the inputed data matches with data which we have in our DB
+    User.findOne({ email: username }, (err, foundUser) => {
+        if (err) {
+            console.log(err);
+        } else {
+            //if user found then check the password if its matches
+            if (foundUser) {
+                if (foundUser.password === password) {
+                    res.render('secrets');
+                }
+            }
+        }
+    });
+});
+
+app.post('/register', (req, res) => {
+    const newUser = new User({
+        email: req.body.username,
+        password: req.body.password
+    });
+
+    newUser.save((err) => {
+        if (err) {
+            console.log(err);
+        } else {
+            res.render('secrets');
+        }
+    });
 });
 //    Listening to port
 app.listen(port, () => {
