13946eee40418a1dd2c38411578932155d3cd25c
abdulazizcode <tajikcoder@gmail.com>
2021-08-26 06:55:36 +0800

    dotenv .env

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
+require('dotenv').config();
 const express = require('express');
 const ejs = require('ejs');
 const mongoose = require('mongoose');
@@ -31,8 +32,10 @@ const userSchema = new mongoose.Schema({
 });
 
 //encrypt password
-const secret = 'Thisisourlittlesecret.';
-userSchema.plugin(encrypt, { secret: secret, encryptedFields: ['password'] });
+userSchema.plugin(encrypt, {
+    secret: process.env.SECRET,
+    encryptedFields: ['password']
+});
 
 const User = new mongoose.model('User', userSchema);
 
