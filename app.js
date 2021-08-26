f03e5c0afccd22406a60974dc1d8d4755eb5be6c
abdulazizcode <tajikcoder@gmail.com>
2021-08-27 00:12:12 +0800

    hashing password instead of encr

--- app.js
+++ app.js
@@ -2,7 +2,8 @@ require('dotenv').config();
 const express = require('express');
 const ejs = require('ejs');
 const mongoose = require('mongoose');
-const encrypt = require('mongoose-encryption');
+//const encrypt = require('mongoose-encryption');
+const md5 = require('md5');
 
 const app = express();
 const port = 3000;
@@ -32,10 +33,10 @@ const userSchema = new mongoose.Schema({
 });
 
 //encrypt password
-userSchema.plugin(encrypt, {
-    secret: process.env.SECRET,
-    encryptedFields: ['password']
-});
+// userSchema.plugin(encrypt, {
+//     secret: process.env.SECRET,
+//     encryptedFields: ['password']
+// });
 
 const User = new mongoose.model('User', userSchema);
 
@@ -51,7 +52,7 @@ app.get('/register', (req, res) => {
 
 app.post('/login', (req, res) => {
     const username = req.body.username;
-    const password = req.body.password;
+    const password = md5(req.body.password);
     //Comparing if the inputed data matches with data which we have in our DB
     User.findOne({ email: username }, (err, foundUser) => {
         if (err) {
@@ -70,7 +71,7 @@ app.post('/login', (req, res) => {
 app.post('/register', (req, res) => {
     const newUser = new User({
         email: req.body.username,
-        password: req.body.password
+        password: md5(req.body.password)
     });
 
     newUser.save((err) => {
